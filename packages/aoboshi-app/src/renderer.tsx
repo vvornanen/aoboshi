32b388436497f74172b626a7b108008186a71f0b
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-05-16 21:10:35 +0300

    build(app): add react

--- packages/aoboshi-app/src/renderer.tsx
+++ packages/aoboshi-app/src/renderer.tsx
@@ -26,7 +26,17 @@
  * ```
  */
 
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { App } from "./App";
 import "./index.css";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 console.log(
   '👋 This message is being logged by "renderer.js", included via Vite'

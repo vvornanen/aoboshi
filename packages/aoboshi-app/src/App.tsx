54c1fb53cb4e00a902052a58a029905f545be13f
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-10-07 22:52:02 +0300

    fix(app): set document language

--- packages/aoboshi-app/src/App.tsx
+++ packages/aoboshi-app/src/App.tsx
@@ -1,15 +1,21 @@
 import "./i18n";
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { darkThemeClass, lightThemeClass } from "./theme/theme.css";
 import { useMediaQuery } from "./useMediaQuery";
 
 export const App = () => {
+  const { i18n } = useTranslation();
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const themeClass = prefersDarkMode ? darkThemeClass : lightThemeClass;
 
   useEffect(() => {
     document.documentElement.className = themeClass;
   }, [prefersDarkMode, themeClass]);
+
+  useEffect(() => {
+    document.documentElement.setAttribute("lang", i18n.language);
+  }, [i18n.language]);
 
   return <div></div>;
 };

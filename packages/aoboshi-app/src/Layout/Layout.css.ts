a4b9dc2c33906c5d9a2c657857fdb4e8882e5531
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-11-10 23:09:33 +0200

    feat(app): add Layout

---
+++ packages/aoboshi-app/src/Layout/Layout.css.ts
@@ -0,0 +1,46 @@
+import { style } from "@vanilla-extract/css";
+import { windowControlsHeight, windowControlsWidth } from "../styles.css";
+import { zIndex } from "../theme/zIndex";
+import { drag } from "../App.css";
+
+export const layout = style({
+  position: "relative",
+  display: "flex",
+  justifyContent: "stretch",
+  height: "100vh",
+});
+
+export const dragRegion = style([
+  drag,
+  {
+    position: "absolute",
+    height: windowControlsHeight,
+    width: "100%",
+    zIndex: -1,
+  },
+]);
+
+export const toggleButton = style({
+  position: "absolute",
+  left: windowControlsWidth,
+  top: `calc(${windowControlsHeight} / 2 - 14px)`,
+  zIndex: zIndex.drawer + 1,
+});
+
+export const toolbar = style({
+  display: "flex",
+  alignItems: "center",
+  height: windowControlsHeight,
+  transitionProperty: "margin-left",
+  transitionDuration: "600ms",
+  transitionTimingFunction: "cubic-bezier(.2,0,0,1)",
+});
+
+export const content = style({
+  paddingLeft: 16,
+  paddingRight: 16,
+  flexGrow: 1,
+  transitionProperty: "margin-left",
+  transitionDuration: "600ms",
+  transitionTimingFunction: "cubic-bezier(.2,0,0,1)",
+});

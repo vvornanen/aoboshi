bd919af411a457a5df1571b75037d15ef5c867e5
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-11-10 21:18:42 +0200

    feat(app): add IconButton

---
+++ packages/aoboshi-app/src/IconButton/IconButton.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { SidebarIcon } from "../icons/SidebarIcon";
+import { IconButton } from "./IconButton";
+
+const meta = {
+  component: IconButton,
+  argTypes: {
+    onClick: { action: true },
+  },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    disabled: false,
+  },
+  render: (args) => (
+    <IconButton {...args}>
+      <SidebarIcon />
+    </IconButton>
+  ),
+};

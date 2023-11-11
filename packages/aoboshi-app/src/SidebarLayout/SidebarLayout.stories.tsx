36f48a706f299830caab47f3928405eee8eac922
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-11-11 18:29:30 +0200

    docs(storybook): add SidebarLayout stories

---
+++ packages/aoboshi-app/src/SidebarLayout/SidebarLayout.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+import {
+  reactRouterParameters,
+  withRouter,
+} from "storybook-addon-react-router-v6";
+import { SidebarLayout as SidebarLayoutComponent } from "./SidebarLayout";
+
+const meta = {
+  component: SidebarLayoutComponent,
+  decorators: [withRouter],
+  parameters: {
+    layout: "fullscreen",
+    reactRouter: reactRouterParameters({
+      location: {},
+      routing: { path: "/" },
+    }),
+  },
+} satisfies Meta<typeof SidebarLayoutComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const SidebarLayout: Story = {};

7c7b9431124559ae16cddb69061941e1cc845931
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-11-20 23:51:04 +0200

    fix(storybook): mock ipc api

--- packages/aoboshi-app/.storybook/preview.ts
+++ packages/aoboshi-app/.storybook/preview.ts
@@ -3,6 +3,13 @@ import { withThemeByClassName } from "@storybook/addon-themes";
 import { darkThemeClass, lightThemeClass } from "../src/theme/theme.css";
 import "../src/styles.css";
 import "../src/i18n.renderer";
+import { IPC_API_KEY, IpcApi } from "../src/ipc";
+
+window[IPC_API_KEY] = {
+  onToggleSidebar: () => {},
+  toggleSidebar: () => {},
+  onNavigate: () => {},
+} satisfies IpcApi;
 
 const preview: Preview = {
   parameters: {

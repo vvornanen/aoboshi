46295ff4bc82c06fc255e51783323ce0ac266148
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-11-20 22:56:51 +0200

    feat(app): add IpcApi

---
+++ packages/aoboshi-app/src/ipc.ts
@@ -0,0 +1,40 @@
+export const IPC_API_KEY = "ipcApi";
+
+export enum IpcEventType {
+  ToggleSidebar = "toggleSidebar",
+  Navigate = "navigate",
+}
+
+/**
+ * Called when the sidebar is toggled from the main process (e.g. application menu)
+ *
+ * @param open true if the sidebar should be opened
+ */
+export type OnToggleSidebarHandler = (open: boolean) => void;
+
+/**
+ * Called when navigation is triggered from the main process (e.g. application menu)
+ *
+ * @param to router path where to navigate
+ */
+export type OnNavigateHandler = (to: string) => void;
+
+/**
+ * Electron IPC renderer-side API to exchange messages with the main process.
+ *
+ * @see https://www.electronjs.org/docs/latest/tutorial/ipc
+ */
+export interface IpcApi {
+  /** Called when the sidebar is toggled from the main process (e.g. application menu) */
+  onToggleSidebar: (callback: OnToggleSidebarHandler) => void;
+
+  /**
+   * Updates the application menu when the sidebar is toggled within the renderer
+   *
+   * @param open true if the sidebar is open
+   */
+  toggleSidebar: (open: boolean) => void;
+
+  /** Called when navigation is triggered from the main process (e.g. application menu) */
+  onNavigate: (callback: OnNavigateHandler) => void;
+}

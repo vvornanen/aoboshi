9084f6b90b0fce1ebb6dbdab79f22d26bad4d83a
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2023-12-10 00:25:31 +0200

    build(app): build jobs separately from the main bundle
    Job files are loaded dynamically in runtime.

--- packages/aoboshi-app/vite.main.config.mjs
+++ packages/aoboshi-app/vite.main.config.mjs
@@ -2,7 +2,14 @@ import fs from "node:fs";
 import path from "node:path";
 import { defineConfig } from "vite";
 
+const jobsDir = "src/jobs";
 const migrationsDir = "src/migrations";
+
+const jobs = fs.readdirSync(jobsDir).filter((file) => {
+  const parsedPath = path.parse(file);
+  return parsedPath.ext === ".ts";
+});
+
 const migrations = fs.readdirSync(migrationsDir).filter((file) => {
   const parsedPath = path.parse(file);
   return parsedPath.ext === ".ts" && parsedPath.name.match(/^\d{2}-/);
@@ -15,6 +22,12 @@ export default defineConfig({
       external: ["better-sqlite3"],
       input: {
         main: "src/main/main.ts",
+        ...Object.fromEntries(
+          jobs.map((file) => [
+            `jobs/${path.parse(file).name}`,
+            path.join(jobsDir, file),
+          ]),
+        ),
         ...Object.fromEntries(
           migrations.map((file) => [
             `migrations/${path.parse(file).name}`,

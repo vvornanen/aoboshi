05c79887f89c3b62935de94162307d8f39826f4c
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-04-07 19:09:50 +0300

    refactor(app): move store provider from renderer.tsx to App

--- packages/aoboshi-app/src/renderer/app/App.tsx
+++ packages/aoboshi-app/src/renderer/app/App.tsx
@@ -1,10 +1,14 @@
 import { FunctionComponent } from "react";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import { ThemeProvider } from "../theme/ThemeProvider";
 import { router } from "./routes";
+import { store } from "./store";
 
 export const App: FunctionComponent = () => (
-  <ThemeProvider>
-    <RouterProvider router={router} />
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </Provider>
 );

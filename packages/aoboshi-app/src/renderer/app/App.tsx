0da5b8870a2d99e058266adb313a48a6bec3a4bd
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-04-07 19:08:36 +0300

    refactor(app): add theme provider

--- packages/aoboshi-app/src/renderer/app/App.tsx
+++ packages/aoboshi-app/src/renderer/app/App.tsx
@@ -1,16 +1,10 @@
-import { useEffect } from "react";
+import { FunctionComponent } from "react";
 import { RouterProvider } from "react-router-dom";
-import { darkThemeClass, lightThemeClass } from "../theme/theme.css";
-import { useMediaQuery } from "../common/useMediaQuery";
+import { ThemeProvider } from "../theme/ThemeProvider";
 import { router } from "./routes";
 
-export const App = () => {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const themeClass = prefersDarkMode ? darkThemeClass : lightThemeClass;
-
-  useEffect(() => {
-    document.documentElement.className = themeClass;
-  }, [prefersDarkMode, themeClass]);
-
-  return <RouterProvider router={router} />;
-};
+export const App: FunctionComponent = () => (
+  <ThemeProvider>
+    <RouterProvider router={router} />
+  </ThemeProvider>
+);
---
+++ packages/aoboshi-app/src/renderer/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent, ReactNode, useEffect } from "react";
+import { useMediaQuery } from "../common/useMediaQuery";
+import { darkThemeClass, lightThemeClass } from "./theme.css";
+
+export type ThemeProviderProps = {
+  children?: ReactNode;
+};
+
+export const ThemeProvider: FunctionComponent<ThemeProviderProps> = ({
+  children,
+}) => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const themeClass = prefersDarkMode ? darkThemeClass : lightThemeClass;
+
+  useEffect(() => {
+    document.documentElement.className = themeClass;
+  }, [prefersDarkMode, themeClass]);
+
+  // For now just add the theme class to the document and return children as is,
+  // but if needed, we could also provide the chosen theme in a context here.
+  return children;
+};

e28d804a74f2f3346346b4594c5c5b72260ba224
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-08-04 05:55:30 +0300

    build: add eslint rule `import/no-relative-parent-imports`

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config(
     plugins: { import: importPlugin },
     rules: {
       eqeqeq: ["error", "always"],
+      "import/no-relative-parent-imports": "error",
       // Copy rules from the recommended config because eslint-plugin-import does not yet support flat config
       // See: https://github.com/import-js/eslint-plugin-import/issues/2948
       ...importPlugin.configs.recommended.rules,

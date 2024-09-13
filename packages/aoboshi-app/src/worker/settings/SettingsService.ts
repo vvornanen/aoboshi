38e0fd72923fda1fff4cf0c049677c73b7334647
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-09-13 09:54:09 +0300

    feat(app): get default settings from environment variables

--- packages/aoboshi-app/src/worker/settings/SettingsService.ts
+++ packages/aoboshi-app/src/worker/settings/SettingsService.ts
@@ -1,17 +1,27 @@
 import { Settings, SettingsRepository } from "~/worker/settings";
-
-const defaultSettings: Settings = {
-  timeZoneConfig: [{ timeZone: "UTC" }],
-};
+import { getEnvironmentVariable } from "~/worker";
 
 export class SettingsService {
   constructor(private settingsRepository: SettingsRepository) {}
 
   getSettings(): Settings {
-    return this.settingsRepository.find() ?? defaultSettings;
+    return this.settingsRepository.find() ?? this.getDefaultSettings();
   }
 
   saveSettings(settings: Settings) {
     this.settingsRepository.save(settings);
+  }
+
+  private getDefaultSettings(): Settings {
+    return {
+      timeZoneConfig: [{ timeZone: "UTC" }],
+      anki: process.env.ANKI_URL
+        ? {
+            url: getEnvironmentVariable("ANKI_URL"),
+            apiKey: getEnvironmentVariable("ANKI_API_KEY"),
+            deckName: getEnvironmentVariable("ANKI_DECK_NAME"),
+          }
+        : undefined,
+    };
   }
 }

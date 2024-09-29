20534293140a97840e2f906c2352772a3cd43905
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2024-09-29 17:58:45 +0300

    feat(app): use grid layout in CharactersCard

--- packages/aoboshi-app/src/renderer/characters/CharactersCard/CharactersCard.css.ts
+++ packages/aoboshi-app/src/renderer/characters/CharactersCard/CharactersCard.css.ts
@@ -1,9 +1,14 @@
 import { style } from "@vanilla-extract/css";
 
 export const charactersCard = style({
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  justifyContent: "start",
-  gap: 8,
+  display: "grid",
+  gridTemplateColumns: "repeat(auto-fill, minmax(40px, 1fr))",
+  justifyContent: "space-between",
+  gap: 4,
+});
+
+export const gridCell = style({
+  aspectRatio: "1 / 1",
+  display: "grid",
+  placeContent: "center",
 });

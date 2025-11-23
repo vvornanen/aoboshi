e873a9c56d458b864588c912690b3e86bc549e79
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2025-11-23 06:15:46 +0200

    build(deps): remove `@chromatic-com/storybook`

--- packages/aoboshi-app/.storybook/main.ts
+++ packages/aoboshi-app/.storybook/main.ts
@@ -11,7 +11,6 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-themes"),
-    getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("storybook-addon-remix-react-router"),
   ],
   framework: {

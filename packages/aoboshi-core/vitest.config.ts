fb7d49efa9a700f7ff251c86b6f174aacefe4b8b
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2025-12-26 17:39:26 +0200

    build: migrate vitest workspace to projects

--- packages/aoboshi-core/vitest.config.ts
+++ packages/aoboshi-core/vitest.config.ts
@@ -2,9 +2,8 @@ import { defineProject } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineProject({
+  plugins: [tsconfigPaths()],
   test: {
     environment: "node",
   },
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  plugins: [tsconfigPaths() as any],
 });

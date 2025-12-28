bbee4f52a3d12ba5550f6bc1641005f69ebf5db3
Ville Vornanen <8843354+vvornanen@users.noreply.github.com>
2025-12-28 21:17:01 +0200

    build: configure vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "v8",
+      include: ["packages/*/src/**/*.{ts,tsx}"],
+      exclude: ["**/index.ts", "**/*.css.ts"],
+    },
     projects: ["packages/*/vitest?(.*).config.ts"],
   },
 });
